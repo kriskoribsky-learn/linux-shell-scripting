eaf659b40fb363290a8ee774b64b2f29fb9f181a
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-05-24 21:25:15 +0200

    feat: jason-cannon-bash-scripting-course finish project-2

--- project-2/add-new-local-user.sh
+++ project-2/add-new-local-user.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # This script creates a new user on the local system.
-# You will be prompted to enter the username (login), the person name, and a password.
+# You must supply a username as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument.
+# A password will be automatically generated for the account.
 # The username, password, and host for the account will be displayed.
 
 # make sure the script is being executed with superuser privileges
@@ -10,10 +12,26 @@ if [[ "${UID}" -ne 0 ]]; then
     exit 1
 fi
 
-# user prompts
-read -p 'Enter the username to create: ' username
-read -p 'Enter the full name of the person that will be using this account: ' fullname
-read -p 'Enter the temporary password to use for the account: ' password
+# make sure the script is called with at least one argument
+if [[ "${#}" -lt 1 ]]; then
+    echo "Usage: ${0} USER_NAME [COMMENT]..."
+    echo 'Please provide at least login name to create an account on the local system.'
+    exit 1
+fi
+
+# save login and remove from argument list
+username="${1}"; shift
+
+# set separator to space and save full name
+fullname="${@}"
+
+# generate pseudo-random password
+password=$(date +%N | sha256sum | head -c48)
+
+if [[ "${?}" -ne 0 ]]; then
+    echo 'Failed to generate random password for the account.'
+    exit 1
+fi
 
 # user account creation
 adduser --comment "${fullname}" --create-home ${username}
